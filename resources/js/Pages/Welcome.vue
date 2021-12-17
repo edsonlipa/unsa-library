16f7b809529cdc096b6b40ab81e5cf4c626c6f9a
Edson Lipa <edson.lipa@gmail.com>
2021-12-17 14:28:01 -0500

    login by google

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -8,9 +8,9 @@
             </Link>
 
             <template v-else>
-                <Link :href="'google'" class="text-sm text-gray-700 underline">
+                <!-- <Link :href="'google'" class="text-sm text-gray-700 underline">
                     Log in
-                </Link>
+                </Link> -->
                 <a
                     href="google"
                     class="inline bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full ">
